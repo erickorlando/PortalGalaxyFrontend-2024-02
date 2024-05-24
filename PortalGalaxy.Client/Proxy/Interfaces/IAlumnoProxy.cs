3e6f55be507b1cbd6e8a6f67e66a55c4ce6d9387
Erick Orlando <erickorlando@outlook.com>
2024-05-23 19:13:07 -0500

    Avance Clase 04 - Parte 01

---
+++ PortalGalaxy.Client/Proxy/Interfaces/IAlumnoProxy.cs
@@ -0,0 +1,10 @@
+﻿using PortalGalaxy.Client.Proxy.Interfaces;
+using PortalGalaxy.Shared.Request;
+using PortalGalaxy.Shared.Response;
+
+namespace PortalGalaxy.Client;
+
+public interface IAlumnoProxy : ICrudRestHelper<AlumnoDtoRequest, AlumnoDtoResponse>
+{
+    Task<ICollection<AlumnoSimpleDtoResponse>> ListarAsync(string? filtro, string? nroDocumento);
+}
